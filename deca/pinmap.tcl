79978cff3ebb88e90784dcc7e921e75228460775
Anderson Ignacio da Silva <anderson@aignacio.com>
2021-05-24 19:47:27 +0100

    Example working with deca board

--- deca/pinmap.tcl
+++ deca/pinmap.tcl
@@ -1,17 +1,23 @@
-
-#set_global_assignment -name FAMILY "MAX 10"
-#set_global_assignment -name DEVICE 10M50DAF484C6GES
-#set_global_assignment -name TOP_LEVEL_ENTITY "top"
-#set_global_assignment -name SDC_FILE DECA.SDC
-#set_global_assignment -name ENABLE_OCT_DONE OFF
-#set_global_assignment -name USE_CONFIGURATION_DEVICE ON
+#============================================================
+# Global definitions for the IOBanks
+#============================================================
+set_global_assignment -name USE_CONFIGURATION_DEVICE ON
+set_global_assignment -name IOBANK_VCCIO 2.5V -section_id 1A
+set_global_assignment -name IOBANK_VCCIO 2.5V -section_id 1B
+set_global_assignment -name IOBANK_VCCIO 2.5V -section_id 2
+set_global_assignment -name IOBANK_VCCIO 3.3V -section_id 3
+set_global_assignment -name IOBANK_VCCIO 3.3V -section_id 4
+set_global_assignment -name IOBANK_VCCIO 1.5V -section_id 5
+set_global_assignment -name IOBANK_VCCIO 1.5V -section_id 6
+set_global_assignment -name IOBANK_VCCIO 1.8V -section_id 7
+set_global_assignment -name IOBANK_VCCIO 1.2V -section_id 8
 
 #============================================================
 # disable config pin so bank8 can use 1.2V
 #============================================================
-#set_global_assignment -name AUTO_RESTART_CONFIGURATION ON
-#set_global_assignment -name ENABLE_CONFIGURATION_PINS OFF
-#set_global_assignment -name ENABLE_BOOT_SEL_PIN OFF
+set_global_assignment -name AUTO_RESTART_CONFIGURATION ON
+set_global_assignment -name ENABLE_CONFIGURATION_PINS OFF
+set_global_assignment -name ENABLE_BOOT_SEL_PIN OFF
 
 #============================================================
 # CLK/ARST
@@ -23,50 +29,39 @@ set_instance_assignment -name IO_STANDARD "1.5 V SCHMITT TRIGGER" -to arst_n
 #============================================================
 # LEDs
 #============================================================
-#1set_location_assignment PIN_C7 -to leds[0]
-#1set_instance_assignment -name IO_STANDARD "1.2 V" -to leds[0]
-#1set_location_assignment PIN_C8 -to leds[1]
-#1set_instance_assignment -name IO_STANDARD "1.2 V" -to leds[1]
-#1set_location_assignment PIN_A6 -to leds[2]
-#1set_instance_assignment -name IO_STANDARD "1.2 V" -to leds[2]
-#1set_location_assignment PIN_B7 -to leds[3]
-#1set_instance_assignment -name IO_STANDARD "1.2 V" -to leds[3]
-#1set_location_assignment PIN_C4 -to leds[4]
-#1set_instance_assignment -name IO_STANDARD "1.2 V" -to leds[4]
-#1set_location_assignment PIN_A5 -to leds[5]
-#1set_instance_assignment -name IO_STANDARD "1.2 V" -to leds[5]
-#1set_location_assignment PIN_B4 -to leds[6]
-#1set_instance_assignment -name IO_STANDARD "1.2 V" -to leds[6]
-#1set_location_assignment PIN_C5 -to heartbeat[0]
-#1set_instance_assignment -name IO_STANDARD "1.2 V" -to heartbeat[0]
-#1#============================================================
-#1# UART TX/RX
-#1#============================================================
-#1set_location_assignment PIN_AB19 -to uart_rxd
-#1set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to uart_rxd
-#1set_location_assignment PIN_V16 -to uart_txd
-#1set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to uart_txd
-#1#============================================================
-#1# Heartbeat Pins
-#1#============================================================
-set_location_assignment PIN_C7 -to heartbeat[0]
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to heartbeat[0]
-#1set_location_assignment PIN_W6 -to heartbeat[1]
-#1set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to heartbeat[1]
-#1set_location_assignment PIN_W8 -to heartbeat[2]
-#1set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to heartbeat[2]
-#1set_location_assignment PIN_AB8 -to heartbeat[3]
-#1set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to heartbeat[3]
-#1#============================================================
-#1# JTAG Pins
-#1#============================================================
-#1set_location_assignment PIN_Y6 -to jtag_tdi
-#1set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to jtag_tdi
-#1set_location_assignment PIN_W7 -to jtag_tdo
-#1set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to jtag_tdo
-#1set_location_assignment PIN_V8 -to jtag_tck
-#1set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to jtag_tck
-#1set_location_assignment PIN_V7 -to jtag_tms
-#1set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to jtag_tms
+set_location_assignment PIN_C7 -to leds[0]
+set_instance_assignment -name IO_STANDARD "1.2 V" -to leds[0]
+set_location_assignment PIN_C8 -to leds[1]
+set_instance_assignment -name IO_STANDARD "1.2 V" -to leds[1]
+set_location_assignment PIN_A6 -to leds[2]
+set_instance_assignment -name IO_STANDARD "1.2 V" -to leds[2]
+set_location_assignment PIN_B7 -to leds[3]
+set_instance_assignment -name IO_STANDARD "1.2 V" -to leds[3]
+set_location_assignment PIN_C4 -to leds[4]
+set_instance_assignment -name IO_STANDARD "1.2 V" -to leds[4]
+set_location_assignment PIN_A5 -to leds[5]
+set_instance_assignment -name IO_STANDARD "1.2 V" -to leds[5]
+set_location_assignment PIN_B4 -to leds[6]
+set_instance_assignment -name IO_STANDARD "1.2 V" -to leds[6]
+set_location_assignment PIN_C5 -to heartbeat[0]
+set_instance_assignment -name IO_STANDARD "1.2 V" -to heartbeat[0]
+#============================================================
+# UART TX/RX
+#============================================================
+set_location_assignment PIN_W18 -to uart_rxd
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to uart_rxd
+set_location_assignment PIN_Y19 -to uart_txd
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to uart_txd
+#============================================================
+# JTAG Pins
+#============================================================
+set_location_assignment PIN_AA20 -to jtag_tdi
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to jtag_tdi
+set_location_assignment PIN_AB21 -to jtag_tdo
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to jtag_tdo
+set_location_assignment PIN_AB19 -to jtag_tck
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to jtag_tck
+set_location_assignment PIN_V16  -to jtag_tms
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to jtag_tms
 
 
--- rtl/top.sv
+++ rtl/top.sv
@@ -6,23 +6,28 @@ module top (
   output              jtag_tdo,
   input               jtag_tck,
   output logic [6:0]  leds,
+  output logic [3:0]  heartbeat,
   input               uart_rxd,
-  output              uart_txd,
-  output logic [3:0]  heartbeat
+  output              uart_txd
 );
   logic [31:0]  gpio_write;
   logic         arst;
-  logic [15:0]  counter_ff;
+  logic [31:0]  counter_ff;
+  logic         heartbeat_ff;
+
+  assign arst = ~arst_n;
+  assign leds = gpio_write[7:0];
+  assign heartbeat = {4{heartbeat_ff}};
 
   always_ff @ (posedge clk_50MHz or posedge arst) begin
     if (arst) begin
-      counter_ff <= '0;
-      heartbeat <= '0;
+      counter_ff    <=  '0;
+      heartbeat_ff  <=  '0;
     end
     else begin
-      if (counter_ff == 'd25000) begin
-        counter_ff <= 'd0;
-        heartbeat <= ~heartbeat;
+      if (counter_ff == 'd25_000_000) begin
+        counter_ff    <=  'd0;
+        heartbeat_ff  <=  ~heartbeat_ff;
       end
       else begin
         counter_ff <= counter_ff+'d1;
@@ -30,22 +35,17 @@ module top (
     end
   end
 
-  assign arst = ~arst_n;
-  assign leds = '0; //gpio_write[7:0];
-  assign jtag_tdo = '0;
-  assign uart_txd = '0;
-
-  //Murax u_murax(
-  //  .io_asyncReset        (arst),
-  //  .io_mainClk           (clk_50MHz),
-  //  .io_jtag_tms          (jtag_tms),
-  //  .io_jtag_tdi          (jtag_tdi),
-  //  .io_jtag_tdo          (jtag_tdo),
-  //  .io_jtag_tck          (jtag_tck),
-  //  .io_gpioA_read        ('0),
-  //  .io_gpioA_write       (gpio_write),
-  //  .io_gpioA_writeEnable (),
-  //  .io_uart_txd          (uart_txd),
-  //  .io_uart_rxd          (uart_rxd)
-  //);
+  Murax u_murax(
+    .io_asyncReset        (arst),
+    .io_mainClk           (clk_50MHz),
+    .io_jtag_tms          (jtag_tms),
+    .io_jtag_tdi          (jtag_tdi),
+    .io_jtag_tdo          (jtag_tdo),
+    .io_jtag_tck          (jtag_tck),
+    .io_gpioA_read        ('0),
+    .io_gpioA_write       (gpio_write),
+    .io_gpioA_writeEnable (),
+    .io_uart_txd          (uart_txd),
+    .io_uart_rxd          (uart_rxd)
+  );
 endmodule
